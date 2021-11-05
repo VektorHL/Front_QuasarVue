3df505faf00f70e2316e1eca06ab3a9ad0aa6d01
VektorHL <vektorhalflife.vektorhl@gmail.com>
2021-11-05 22:13:16 +0300

    тублары и главный заголовок готовы

---
+++ src/pages/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div data-v-7c94da36="" class="q-pa-md q-ml-sm q-mt-xl">
+	<div class="row title">
+		<div class="col-12" style="margin-right:10%;">
+			<p class="bigFont colorGrey text-weight-bold">Основные показатели<br> о ходе субсидийной кампании</p>
+		</div>
+	</div>
+  <!-- <CurrentFundsAmount /> -->
+  
+
+
+</div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
